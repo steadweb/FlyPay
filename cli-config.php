f160665e8d58e85e4f3b36e0f3d548520be39627
Luke Steadman <luke.steadman@jadu.co.uk>
2016-12-30 13:23:08 +0000

    Added dotenv to cli config

--- cli-config.php
+++ cli-config.php
@@ -4,6 +4,10 @@ use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 require __DIR__ . '/vendor/autoload.php';
 
+// Dotenv setup
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 $settings = require __DIR__ . '/app/settings.php';
 $app = new \Slim\App($settings);
 require __DIR__ . '/app/dependencies.php';
