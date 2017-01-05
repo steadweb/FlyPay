e286ce9b38c399c94976cb98de2146f082a7c755
Luke Steadman <luke.steadman@jadu.co.uk>
2017-01-05 16:16:22 +0000

    Added card entity test

---
+++ tests/CardEntityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Steadweb\Flypay\Tests;
+
+use Steadweb\Flypay\Entities\Card;
+
+class CardEntityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var Card
+     */
+    private $card;
+
+    public function setUp()
+    {
+        $card = new Card();
+
+        $this->id = uniqid();
+
+        $card->setId($this->id);
+        $card->setLast4('1234');
+        $card->setType('VISA');
+
+        $this->card = $card;
+    }
+
+    public function testSetAndGetValues()
+    {
+        $this->assertSame($this->id, $this->card->getId());
+        $this->assertSame('1234', $this->card->getLast4());
+        $this->assertSame('VISA', $this->card->getType());
+    }
+
+    public function testJsonSerializeReturnsCorrectValues()
+    {
+        $arr = [
+            'id' => $this->card->getId(),
+            'last4' => '1234',
+            'type' => 'VISA'
+        ];
+
+        $this->assertSame($arr, $this->card->jsonSerialize());
+    }
+}
